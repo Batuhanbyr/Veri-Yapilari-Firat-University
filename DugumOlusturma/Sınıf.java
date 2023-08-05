cbfae56cb81e7d56ff0efab95617e039cd6b6568
Batuhan Bayır <95686987+Batuhanbyr@users.noreply.github.com>
2023-08-05 22:06:53 +0300

    3. Commit

--- DugumOlusturma/Sınıf.java
+++
@@ -1,7 +0,0 @@
-package DugumOlusturma;
-
-public class Sınıf {
-int sayı;
-String isim;
-Sınıf next; // gösterici oluşturuyoruz (sınıf ismi ile aynı ve next olmalı bir düğüm oluşturabilmek için)
-}
